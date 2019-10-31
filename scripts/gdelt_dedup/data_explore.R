d6aed6f941ecaf954853a0a980c5df8299542360
Andee Kaplan <andee.kaplan@gmail.com>
2019-10-31 14:15:46 -0600

    track files

--- scripts/gdelt_dedup/data_explore.R
+++ scripts/gdelt_dedup/data_explore.R
@@ -2,8 +2,28 @@
 library(tidyverse)
 
 # load data
-gdelt <- read_csv("data/gdelt/westafrica_conflict_evts_1979-2019_gdelt.csv")
+gdelt <- read_csv("data/gdelt/westafrica_conflict_evts_1979-2019_gdelt.csv") %>%
+  filter(Year != "Year")
 
 dim(gdelt)
 
 head(gdelt)
+
+# potential blocking
+gdelt %>%
+  group_by(Year, ActionGeo_CountryCode) %>%
+  summarise(count = n()) %>%
+  group_by(count) %>%
+  ggplot() +
+  geom_histogram(aes(count))
+
+gdelt %>%
+  group_by(Year, ActionGeo_CountryCode) %>%
+  summarise(count = n()) %>%
+  group_by(count) %>%
+  filter(count > 10000)
+
+gdelt %>%
+  group_by(GLOBALEVENTID) %>%
+  mutate(count = n()) %>%
+  filter(count > 1)
